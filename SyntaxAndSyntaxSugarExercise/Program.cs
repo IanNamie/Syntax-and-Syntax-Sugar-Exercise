f9b1e0af9c8fc0f8f9930072ce46aced41e24fce
Ian Namie <Iannamie82@gmail.com>
2023-02-16 14:59:53 -0600

    updated completed exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -11,14 +11,8 @@
                 and the ternary operator*/
 
             var answer = 4;
-            string response;
-            if (answer < 9)
-            {
-                response = "answer is less than nine";
-            }
-            else
-            {
-                response = "answer is greater than or equal to nine";
+           var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine"; 
+
             }
         }
     }
